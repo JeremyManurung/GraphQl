cc55280949d4d01848677541778f9be93f0a15ed
JeremyManurung <87016966+JeremyManurung@users.noreply.github.com>
2021-09-23 07:00:47 +0700

    Memasukkan subscription

--- src/component/Apollo_client.js
+++ src/component/Apollo_client.js
@@ -1,17 +1,52 @@
 import {
   ApolloClient,
   InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
 } from "@apollo/client";
 
-const client = new ApolloClient({
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+
+const httpLink = new HttpLink({
   uri: 'https://kampusmerdeka.hasura.app/v1/graphql',
-  cache: new InMemoryCache(),
   headers: {
       'x-hasura-admin-secret' : 'GJyFmwaJfr1RRinrd2T40xq6njF28njBZ4wb5mrJ5xTVo05p4uHy3iRRK6CpJwOk'
   }
+});
+
+const wsLink = new WebSocketLink({
+  uri: 'wss://kampusmerdeka.hasura.app/v1/graphql',
+  options: {
+    reconnect: true,
+    connectionParams : {
+      headers: {
+      'x-hasura-admin-secret' : 'GJyFmwaJfr1RRinrd2T40xq6njF28njBZ4wb5mrJ5xTVo05p4uHy3iRRK6CpJwOk'
+      }
+    }
+  }
+});
+
+
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+
+
+const client = new ApolloClient({
+  link:splitLink,
+  cache: new InMemoryCache(),
+
 });
 
 export default client
