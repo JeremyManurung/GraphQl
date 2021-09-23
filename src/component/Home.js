d8f6de5b7280c35a8c74354168fe926e4ac71b8d
JeremyManurung <87016966+JeremyManurung@users.noreply.github.com>
2021-09-23 07:02:48 +0700

    Ganti useQuery ke UseSubscription

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,62 +3,75 @@ import { v4 as uuidv4 } from "uuid";
 import PassengerInput from './PassengerInput';
 import ListPassenger from './ListPassenger';
 import Header from './Header';
-import {gql, useQuery, useLazyQuery, useMutation} from '@apollo/client';
+import {gql, useQuery, useLazyQuery, useMutation, useSubscription} from '@apollo/client';
 import { useState } from 'react';
 import LoadingSvg from "./LoadingSvg"
 
 const GetData = gql`
-    query MyQuery {
-        anggota {
-          id
-          jenis_kelamin
-          nama
-          umur
-        }
-      }      
-    `
-
-const deleteData = gql`
-mutation MyMutation2($id: Int!) {
-    delete_anggota_by_pk(id: $id) {
-      id
-    }
-  }
-`
-
-const insertData = gql`
-mutation MyMutation($jenis_kelamin: String!, $nama: String! $umur: Int!, $id: Int!) {
-    insert_anggota(objects: {jenis_kelamin: $jenis_kelamin, nama: $nama, umur: $umur, id: $id}) {
-      returning {
-        id
-      }
-    }
-  }
-`
-
-
-const GetDataByUserId = gql `
-        query MyQuery($id: Int!) {
-        anggota(where: {id: {_eq: $id}}) {
+        query MyQuery {
+            anggota {
+            id
+            jenis_kelamin
             nama
             umur
-            jenis_kelamin
+            }
+        }      
+        `
+
+    const insertData = gql`
+    mutation MyMutation($jenis_kelamin: String!, $nama: String! $umur: Int!, $id: Int!) {
+        insert_anggota(objects: {jenis_kelamin: $jenis_kelamin, nama: $nama, umur: $umur, id: $id}) {
+        returning {
             id
         }
         }
-    `;
+    }
+    `
+
+    const deleteData = gql`
+    mutation MyMutation2($id: Int!) {
+        delete_anggota_by_pk(id: $id) {
+        id
+        }
+    }
+    `
+    const GetDataByUserId = gql `
+            query MyQuery($id: Int!) {
+            anggota(where: {id: {_eq: $id}}) {
+                nama
+                umur
+                jenis_kelamin
+                id
+            }
+            }
+        `;
+
+    const subscriptionData = gql`
+        subscription MySubscription {
+        anggota {
+            jenis_kelamin
+            nama
+            umur
+            id
+        }
+    }
+    `
 
 function Home () {
     
     const [list, setList] = useState([])
-    const {data, loading, error, refetch} = useQuery(GetData)
-
+    // const {data, loading, error, refetch} = useQuery(GetData)
     const [insertDataN, {loading:loadingInsert}] = useMutation(insertData, {
         refetchQueries: [GetData]
     });
     const [deleteDataN, {loading : loadingDelete}] = useMutation(deleteData,{
         refetchQueries: [GetData]
     });
+
+    const {data, loading, error} = useSubscription(subscriptionData)
+
+
+    
     if(loading || loadingInsert || loadingDelete ){
         return <LoadingSvg/>
     }
@@ -79,14 +92,14 @@ function Home () {
         const newData = {
             ...newUser
         }
-        // console.log(newData)
         insertDataN({variables :{
-              id: newData.id,
-              nama: newData.nama,
-              umur: newData.umur,
-              jenis_kelamin: newData.jenisKelamin
+            id: newData.id,
+            nama: newData.nama,
+            umur: newData.umur,
+            jenis_kelamin: newData.jenisKelamin
         }})
     }
+    
     // const onGetData = () =>{
     //     getData_qry({variables : {
     //         id : input
@@ -99,10 +112,10 @@ function Home () {
     // const onchangeInput = (e) => {
     //     setInput(e.target.value)
     // }
+    
     return(
         <div>
             <Header />
-            
             {/* <input type="text" onChange={onchangeInput} /> */}
             {/* <button onClick={onGetData}>Get Data</button> */}
             <ListPassenger data={data?.anggota} hapusPengunjung={hapusPengunjung} />
